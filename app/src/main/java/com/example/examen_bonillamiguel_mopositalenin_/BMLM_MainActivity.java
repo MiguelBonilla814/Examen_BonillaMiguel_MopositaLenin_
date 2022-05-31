0fbd364ac438bd053deb4dbf3a647e30c0760c22
Miguel Enrique Bonilla Quishpe <64868918+MiguelBonilla814@users.noreply.github.com>
2022-05-30 19:02:04 -0500

    Fecha 30/05/2022 Hora 19:01

--- app/src/main/java/com/example/examen_bonillamiguel_mopositalenin_/BMLM_MainActivity.java
+++ app/src/main/java/com/example/examen_bonillamiguel_mopositalenin_/BMLM_MainActivity.java
@@ -1,19 +1,26 @@
 package com.example.examen_bonillamiguel_mopositalenin_;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class BMLM_MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
     }
+
+
 }
---
+++ app/src/main/java/com/example/examen_bonillamiguel_mopositalenin_/BMLM_SegundaActivity.java
@@ -0,0 +1,55 @@
+package com.example.examen_bonillamiguel_mopositalenin_;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class BMLM_SegundaActivity extends AppCompatActivity {
+
+    private ArrayList<Integer> listaNumeros = new ArrayList<>();
+    private ListView list;
+    private EditText edit_agregar_numero;
+    private ArrayAdapter<Integer> adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bmlm_segunda);
+
+        edit_agregar_numero = findViewById(R.id.editText_agregar);
+        list = findViewById(R.id.list_view);
+
+    }
+
+    public void Agregar(View view){
+        listaNumeros.add(Integer.parseInt(edit_agregar_numero.getText().toString()));
+        edit_agregar_numero.setText("");
+        Toast.makeText(this, String.valueOf(listaNumeros.size()), Toast.LENGTH_LONG).show();
+        adapter = new ArrayAdapter<Integer>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, listaNumeros);
+        list.setAdapter(adapter);
+    }
+
+    public void Cerrar(View view){
+        int[] lista = new int[listaNumeros.size()];
+
+        for (int i = 0; i < lista.length; i++){
+            lista[i] = listaNumeros.get(i);
+        }
+
+        Intent intent = new Intent();
+        intent.putExtra("vectorOriginal", lista);
+        setResult(Activity.RESULT_OK, intent);
+        finish();
+    }
+
+}
